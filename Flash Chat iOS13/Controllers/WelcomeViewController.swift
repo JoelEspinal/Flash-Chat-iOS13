d5591fea02af7a3637e1b8aa3ca2096416e3d429
JoelEspinal <joel_espinal@claro.com.do>
2024-03-08 16:48:25 -0400

    Style navigation

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -17,6 +17,16 @@ class WelcomeViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     var charIndex = 0.0
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
